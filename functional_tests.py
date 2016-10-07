7589842027d3d12e650a2706306a3eac41102844
Olus202 <aleksandrabacik@gmail.com>
2016-10-07 10:04:37 +0200

    Functional tests of polish and english links

--- functional_tests.py
+++ functional_tests.py
@@ -7,18 +7,57 @@ class VisitingTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.get('http://localhost:63342/PycharmProjects/my_cv_site/home.html')
-#        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(3)
 
-    def test_run_home_page_and_check_title(self):
-        self.assertIn('Witam', self.driver.title)
-
-    def test_run_menu_pages(self):
+    def test_run_menu_page_check_title_n_contents_back_to_home(self):
         pages = ['O mnie', 'Wykształcenie', 'Systemy', 'Programowanie', 'Kontakt']
         for p in pages:
-            link = self.driver.find_element_by_class_name('menu').find_element_by_partial_link_text(p)
-            link.click()
-
+            page = self.driver.find_element_by_class_name('menu').find_element_by_partial_link_text(p)
+            page.click()
             self.assertIn(p, self.driver.title)
+
+            contents = self.driver.find_element_by_id('contents').text
+            self.assertIn(p, contents)
+
+            back = self.driver.find_element_by_id('logo')
+            back.click()
+            self.assertIn('Aleksandra', self.driver.title)
+
+    def test_run_english_page_check_title_n_contents_back_to_home(self):
+        english = self.driver.find_element_by_partial_link_text('english')
+        english.click()
+
+        pages = ['About me', 'Education', 'Systems', 'Web', 'Contact']
+        for p in pages:
+            page = self.driver.find_element_by_class_name('menu').find_element_by_partial_link_text(p)
+            page.click()
+            self.assertIn(p, self.driver.title)
+
+            contents = self.driver.find_element_by_id('contents').text
+            self.assertIn(p, contents)
+
+            back = self.driver.find_element_by_id('logo')
+            back.click()
+            self.assertIn('Aleksandra', self.driver.title)
+
+    def test_run_page_run_english_check_titles(self):
+        pages = ['O mnie', 'Wykształcenie', 'Systemy', 'Programowanie', 'Kontakt']
+        index = 0
+        for p in pages:
+            page = self.driver.find_element_by_class_name('menu').find_element_by_partial_link_text(p)
+            page.click()
+            self.assertIn(p, self.driver.title)
+
+            english = self.driver.find_element_by_partial_link_text('english')
+            english.click()
+
+            title = self.driver.title
+            english_titles = ['About me', 'Education', 'Systems', 'Web', 'Contact']
+            self.assertIn(english_titles[index], title)
+
+            polish = self.driver.find_element_by_partial_link_text('polski')
+            polish.click()
+            index += 1
 
     def test_run_linkedin_link(self):
         link = self.driver.find_element_by_class_name("icon-linkedin-circled")
@@ -40,9 +79,6 @@ class VisitingTest(unittest.TestCase):
 
         self.driver.switch_to.window(self.driver.window_handles[1])
         self.assertIn("poza drzwiami", self.driver.title)
-
-    def test_end(self):
-        self.fail('End of test')
 
     def tearDown(self):
         self.driver.quit()
